df38cb72847118a91a6e34f488f9715d7308504d
KhakhulinAndrey <leadtek63@gmail.com>
2017-09-22 19:45:55 +0400

    AuthorApp добавил поиск

--- src/main/java/ru/khakhulin/books/ui/authors/AuthorApp.java
+++ src/main/java/ru/khakhulin/books/ui/authors/AuthorApp.java
@@ -4,7 +4,7 @@ import ru.khakhulin.books.dao.AuthorDAO;
 import ru.khakhulin.books.model.Author;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
+
 import javax.swing.table.AbstractTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -15,9 +15,9 @@ import java.util.List;
  */
 public class AuthorApp extends JFrame {
 
-    private JPanel contentPane;
     private JTextField lastNameTextField;
     private JButton btnSearch;
+    private JButton allBooks;
     private JScrollPane scrollPane;
     private JTable table;
 
@@ -28,6 +28,7 @@ public class AuthorApp extends JFrame {
             public void run() {
                 try {
                     AuthorApp frame = new AuthorApp();
+                    frame.setLocationRelativeTo(null);
                     frame.setVisible(true);
 
                 } catch (Exception e) {
@@ -46,22 +47,19 @@ public class AuthorApp extends JFrame {
 
         setTitle("Авторы");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 450, 300);
-        contentPane = new JPanel();
-        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        contentPane.setLayout(new BorderLayout(0, 0));
-        setContentPane(contentPane);
+        setDefaultLookAndFeelDecorated(true);
+        setSize(500, 400);
+        setLayout(new FlowLayout());
 
-        JPanel panel = new JPanel();
-        FlowLayout flowLayout = (FlowLayout) panel.getLayout();
-        flowLayout.setAlignment(FlowLayout.LEFT);
-        contentPane.add(panel, BorderLayout.NORTH);
+
+
+
 
         JLabel lblEnterLastName = new JLabel("Введите фамилию");
-        panel.add(lblEnterLastName);
+        add(lblEnterLastName);
 
         lastNameTextField = new JTextField();
-        panel.add(lastNameTextField);
+        add(lastNameTextField);
         lastNameTextField.setColumns(10);
 
         btnSearch = new JButton("Поиск");
@@ -69,7 +67,6 @@ public class AuthorApp extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 try {
                     String surname = lastNameTextField.getText();
-
                     List<Author> authors = null;
 
                     if (surname != null && surname.trim().length() > 0) {
@@ -78,16 +75,9 @@ public class AuthorApp extends JFrame {
                         authors = authorDAO.getAll();
                     }
 
-                    // create the model and update the "table"
                     AbstractTableModel model = new AuthorTableModel(authors);
-
                     table.setModel(model);
 
-					/*
-					for (Employee temp : employees) {
-						System.out.println(temp);
-					}
-					*/
                 } catch (Exception exc) {
                     JOptionPane.showMessageDialog(AuthorApp.this, "Error: " + exc, "Error", JOptionPane.ERROR_MESSAGE);
                 }
@@ -95,12 +85,18 @@ public class AuthorApp extends JFrame {
             }
         });
 
-        panel.add(btnSearch);
+        add(btnSearch);
+
+        allBooks = new JButton("Все книги");
+        add(allBooks);
 
         scrollPane = new JScrollPane();
-        contentPane.add(scrollPane, BorderLayout.CENTER);
+        add(scrollPane, BorderLayout.CENTER);
 
+        AbstractTableModel model = new AuthorTableModel(authorDAO.getAll());
         table = new JTable();
+        table.setModel(model);
         scrollPane.setViewportView(table);
+
     }
 }
